c3775842097eec22b06915482cef0e2ee66f90f3
carlave117 <62717654+carlave117@users.noreply.github.com>
2021-06-11 19:25:29 -0600

    Creación modelo Estudiante

---
+++ src/java/models/Estudiante.java
@@ -0,0 +1,64 @@
+package models;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Paola Hernandez
+ */
+public class Estudiante {
+    
+        private int carnet;
+        private String nombre;
+        private String apellido;
+        private String correo;
+        private int carrera;
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(int carrera) {
+        this.carrera = carrera;
+    }
+    
+        
+        
+}
